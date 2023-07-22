5de972f4d961f3cf150305dcbfeb7bb3f389dac9
Sam <samuelgardner101@gmail.com>
2023-07-22 02:33:04 +0100

    Missing token

--- src/main/java/org/intellij/sdk/language/psi/SppTokenSets.java
+++ src/main/java/org/intellij/sdk/language/psi/SppTokenSets.java
@@ -6,7 +6,8 @@ public interface SppTokenSets {
     TokenSet STRING_LITERALS = TokenSet.create(SppTypes.LX_SNG_QUOTE_CHR, SppTypes.LX_DOUBLE_QUOTE_STR);
     TokenSet COMMENTS = TokenSet.create(SppTypes.BLOCK_COMMENT, SppTypes.LINE_COMMENT);
     TokenSet KEYWORDS = TokenSet.create(
-            SppTypes.KW_MOD, SppTypes.KW_USE, SppTypes.KW_ENUM, SppTypes.KW_IN, SppTypes.KW_FN, SppTypes.KW_MUT, SppTypes.KW_LET, SppTypes.KW_IF,
+            SppTypes.KW_MOD, SppTypes.KW_USE, SppTypes.KW_ENUM, SppTypes.KW_IN, SppTypes.KW_FN, SppTypes.KW_GN,
+            SppTypes.KW_MUT, SppTypes.KW_LET, SppTypes.KW_IF,
             SppTypes.KW_ELSE, SppTypes.KW_WHILE, SppTypes.KW_FOR, SppTypes.KW_DO,
             SppTypes.KW_RET, SppTypes.KW_YIELD, SppTypes.KW_CLS, SppTypes.KW_WHERE, SppTypes.KW_TRUE,
             SppTypes.KW_FALSE, SppTypes.KW_AS, SppTypes.KW_SUP, SppTypes.KW_WITH, SppTypes.KW_BREAK, SppTypes.KW_CONT,
